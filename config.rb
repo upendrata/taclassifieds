7cc1f05099996a87efe9028e0c8e130c8c99fce9
Upendra <uvuyyuru@hertz.com>
2015-04-20 20:59:54 +0530

    workspace configurations

---
+++ config.rb
@@ -0,0 +1,7 @@
+# Set this to the root of your project when deployed:
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Classifieds</title>
+		<link href="css/global.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<div class="container">
+			<div class="headerContainer">Header Goes over here...</div>
+			<div id="pageContainer">Body of the page goes over here...</div>
+			<div class="footerContainer">Copy rights info goes over here...</div>
+			<script type="text/javascript" src="js/lib/jquery-1.11.2.min.js"></script>
+			<script type="text/javascript" src="js/lib/underscore-min.js"></script>
+			<script type="text/javascript" src="js/lib/backbone-min.js"></script>
+		</div>
+	</body>
+</html>
