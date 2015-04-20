d7553856aa5b6c68a4c3a24fa23ceaa7602aed25
Upendra <uvuyyuru@hertz.com>
2015-04-20 21:19:31 +0530

    db changes

---
+++ info/taclassifieds.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 20, 2015 at 05:48 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `taclassifieds`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taclassifiedusers`
+--
+
+CREATE TABLE IF NOT EXISTS `taclassifiedusers` (
+  `empid` int(4) NOT NULL,
+  `empname` varchar(200) NOT NULL,
+  `empemail` varchar(200) NOT NULL,
+  `emppassword` varchar(25) NOT NULL,
+  PRIMARY KEY (`empid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
