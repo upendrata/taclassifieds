765f1c77c8699b892a36d9b945e00311d417e13b
Upendra <uvuyyuru@hertz.com>
2015-04-21 19:17:43 +0530

    login functionality and fetch data for home page

--- codebase/getClassifiedsForHomePage.php
+++ codebase/getClassifiedsForHomePage.php
@@ -1,28 +1,28 @@
 <?php
 	@include('dbConnect.php');
 	
-	$obj = new classifiedsUser();
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+	$obj = new classifieds();
 	// Define HTTP responses
     $http_response_code = array(200 => 'OK', 400 => 'Bad Request', 401 => 'Unauthorized', 403 => 'Forbidden', 404 => 'Not Found', 405 => 'Error');
 	header('Content-Type: application/json; charset=utf-8');
 	
-	return $obj->validateUser($username, $password, $http_response_code);
+	return $obj->getClassifieds($http_response_code);
 	
-	class classifiedsUser{
-		public function validateUser($uname, $pwd, $http_response_code){
+	class classifieds{
+		public function getClassifieds($http_response_code){
 			$flag = false;
 			$dbConnect = new taClassifiedDBConnect();
 			if($dbConnect->authenticateDB()){
 				if($dbConnect->connectToDB()){
-					$search_query = "select * from taclassifiedusers";
+					$search_query = "select * from taclassifieds where classifiedDisplay = 1 order by classifiedId";
 					$search_result = mysql_query($search_query);
+					$classifiedsData = array();  $classifiedItem = array(); $count=0;
 					while ($search_row = mysql_fetch_array($search_result)) {
-						if($uname == $search_row['empemail'] && $pwd == $search_row['emppassword']){
-							$flag = true;
-							break;
-						}
+						$flag = true;
+						$classifiedItem['empemail'] = $search_row['empemail'];
+						$classifiedItem['classifiedHeading'] = $search_row['classifiedHeading'];
+						$classifiedItem['classifiedCategory'] = $search_row['classifiedCategory'];
+						$classifiedsData[$count++] = $classifiedItem;
 					}
 				}else{
 					return "false";
@@ -35,15 +35,15 @@
 				$response['status'] = 200;
 				header('HTTP/1.1 '.$response['status'].' '.$http_response_code[ $response['status'] ]);
 				
-				$res = '{"status":true, "url":"classifieds"}';
+				echo json_encode($classifiedsData);
 			}else{
 				 // Set HTTP Response
 				$response['status'] = 404;
 				header('HTTP/1.1 '.$response['status'].' '.$http_response_code[ $response['status'] ]);
 				
-				$res = '{"status": false, "responseText": "Username and Password are incorrect!..."}';
+				$res = '{"status": false, "responseText": "Something went wrong not able to fetch Data!..."}';
+				echo $res;
 			}
-			echo $res;
 		}
 	}
 ?>
